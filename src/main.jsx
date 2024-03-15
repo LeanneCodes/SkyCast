6faf2e2eca4dba69f0f12a1cd78d1c8a0f70b5bd
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-03-15 15:02:21 +0000

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Navbar from './Containers/Navbar/Navbar.jsx'
-import Footer from './Containers/Footer/Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
     <App />
-    <Footer />
   </React.StrictMode>,
 )
